452f57976a135c8b72fc8d8f580d5d50618c0cae
Austin Henriksen <austin@zeroc.com>
2019-10-30 05:14:29 -0400

    Remove checksum support (#607)
    * Removed checksum sources from swift mapping project file.
    
    * Removed checksum tests from Python.
    
    * Removed checksum from Python mapping.
    
    * Removed checksum tests from PHP mapping.
    
    * Removed checksum support from PHP mapping.
    
    * Removed checksum sources from MATLAB project file.
    
    * Removed checksum option from slice2x manpages.
    
    * Removed checksum sources from JavaScript mapping.
    
    * Removed checksum tests from Java mapping.
    
    * Removed outdated checksum metadata from Java mapping.
    
    * Removed checksum support from Java mapping.
    
    * Removed checksum tests from csharp.
    
    * Removed checksum support from csharp mapping.
    
    * Removed checksum tests from cpp
    
    * Removed checksum support from IceBox.
    
    * Removed checksum support from IceStorm.
    
    * Removed Slice checksum support from IceGrid.
    
    * Fixed broken filters file for Slice VS project.
    
    * Removed checksum support from cpp mapping.
    
    * Removed checksum support from Slice definitions.
    
    * Removed checksum support from cpp98 mapping.

--- csharp/src/IceBox/ServiceManagerI.cs
+++ csharp/src/IceBox/ServiceManagerI.cs
@@ -48,12 +48,6 @@ class ServiceManagerI : ServiceManagerDisp_
         _argv = args;
         _traceServiceObserver = _communicator.getProperties().getPropertyAsInt("IceBox.Trace.ServiceObserver");
     }
-
-    public override Dictionary<string, string> getSliceChecksums(Ice.Current current)
-    {
-        return Ice.SliceChecksums.checksums;
-    }
-
     public override void startService(string name, Ice.Current current)
     {
         ServiceInfo info = new ServiceInfo();
